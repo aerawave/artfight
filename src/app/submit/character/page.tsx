66fcdedb44d0af9bfeaca4923bd1bcebdd99f0ca
aerawave <aera@aerawave.com>
2024-07-26 20:44:01 -0400

    Added some margin

--- src/app/submit/character/page.tsx
+++ src/app/submit/character/page.tsx
@@ -18,7 +18,7 @@ export default async function SubmitCharacterPage() {
             <Navigation crumbs={[HomeCrumb, ...local_crumbs]} />
             <div className="flex flex-col gap-4">
                 <h1 className="text-4xl">Submit Character</h1>
-                <div className="rounded-md bg-cyan-600 p-3 text-xl text-white">
+                <div className="rounded-md bg-cyan-600 p-3 text-xl text-white m-2">
                     <p>
                         All fields with an asterisk need to be filled in. BBCode
                         can be used in text area fields.
